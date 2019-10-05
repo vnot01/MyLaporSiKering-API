fee61d837fb78156d6dd0842bbeafaa6199f5fd1
vnot01 <56180550+vnot01@users.noreply.github.com>
2019-10-05 10:44:34 +0700

    API

---
+++ include/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost:3306");
+define("DB_USER", "root_laporkering");
+define("DB_PASSWORD", "f3rifebDR4GONLISTIO");
+define("DB_DATABASE", "laporkering");
+?>
